a5a38abbb4bd6882318e175cb71ed28030cfd3db
yusuf <yusufyn04@gmail.com>
2022-04-14 00:04:58 +0200

    Added Explanation

--- 14_Longest_Common_Prefix.py
+++ 14_Longest_Common_Prefix.py
@@ -1,10 +1,18 @@
+### Goal is to find as many similar letters as possible in all the given words
+### Ex: given_words = ["flower", "flow"< "flight]
+### Result = "fl"
+### Ex: given_words = ["dog", "racecar"]
+### Result = ""
+################################################################################
 given_words = ["flower", "flow", "flight"]
 
 def find_prefix():
     result = ""
-    for i in range(len(given_words[0])):
+    for i in range(len(given_words[0])):     ### loops through given_words.length times
         for word in given_words:
-            if len(word) == i or word[i] != given_words[0][i]:
+            if len(word) == i or word[i] != given_words[0][i]:  ### checks if the current word has less letters than the i'th loop
+                                                                ### or check if the current words i'th letter is not equal to the
+                                                                ### first word of the given_words
                 return result
         result += given_words[0][i]
     return result
